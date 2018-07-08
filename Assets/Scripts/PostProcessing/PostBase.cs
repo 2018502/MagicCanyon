cf422d57f08203c8f74bf32c7186ac401bb3aa31
sunzhiyuanabcd <18166454868@163.com>
2018-07-08 17:29:57 +0800

    add files

---
+++ Assets/Scripts/PostProcessing/PostBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
